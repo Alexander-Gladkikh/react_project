d6c91d0d285d17eb27af22064a458024190208fd
Alexander <som17son25@gmail.com>
2023-01-07 13:09:45 +0300

    create controlled input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, {useState} from 'react'
 function App() {
 
     const [count, setCount] = useState(0)
+    const [value, setValue] = useState('')
 
     function increment() {
         setCount(count + 1)
@@ -17,6 +18,12 @@ function App() {
   return (
     <div className="App">
         <h1>{count}</h1>
+        <h1>{value}</h1>
+        <input
+            type="text"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+        />
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>Decrement</button>
 
