2e71408f45c607f07d85cd868b75fda36abb04e7
Alexander <som17son25@gmail.com>
2023-01-07 13:20:29 +0300

    create functional component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React, {useState} from 'react'
+import Counter from "./components/Counter";
 
 
 
 function App() {
 
-    const [count, setCount] = useState(0)
-    const [value, setValue] = useState('')
-
-    function increment() {
-        setCount(count + 1)
-    }
-    function decrement() {
-        setCount(count - 1)
-    }
-
-
   return (
     <div className="App">
-        <h1>{count}</h1>
-        <h1>{value}</h1>
-        <input
-            type="text"
-            value={value}
-            onChange={event => setValue(event.target.value)}
-        />
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-
+        <Counter/>
     </div>
   );
 }
