a3c0978a0eb09697ca50f270cb2e823dae964f98
Alexander <som17son25@gmail.com>
2023-01-14 19:06:23 +0300

    added React transition group

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({post, title, removePost}) => {
 
-    if(!post.length) {
+    if (!post.length) {
         return <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
     }
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {post.map((post, index) => {
-                return <PostItem removePost={removePost} number={index + 1} post={post} key={post.id}/>})}
+            <TransitionGroup>
+                {post.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames='post'>
+                        <PostItem
+                            removePost={removePost}
+                            number={index + 1}
+                            post={post}
+                        />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
