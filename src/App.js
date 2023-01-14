f2026a52072171f8fce6c1c14d40214f4f6bc4a9
Alexander <som17son25@gmail.com>
2023-01-14 14:21:56 +0300

    create component MySelect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {useRef, useState} from 'react'
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/UI/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'JavaScript', body: 'Description'},
-        {id: 2, title: 'JavaScript 2', body: 'Description'},
-        {id: 3, title: 'JavaScript 3', body: 'Description'}
+        {id: 1, title: 'JavaScript', body: 'AAAA'},
+        {id: 2, title: 'React', body: 'SSSSS'},
+        {id: 3, title: 'Redux', body: 'DDDD'}
     ])
+
+    const [selectedSort, setSelectedSort] = useState()
 
     const create = (newPost) => {
         setPosts([...posts, newPost])
@@ -20,11 +20,30 @@ function App() {
         setPosts(posts.filter(el => el.id !== post.id))
     }
 
+    const sortPost = (sort) => {
+        console.log(sort)
+        setSelectedSort(sort)
+        setPosts([...posts.sort((a,b) => a[sort].localeCompare(b[sort]))])
+    }
 
-    return (<div className="App">
+    return (
+        <div className="App">
         <PostForm create={create}/>
-        <PostList removePost={removePost} post={posts} title={'Список постов'}/>
-    </div>);
+        <hr style={{margin: '15px 0'}}/>
+        <MySelect
+            value={selectedSort}
+            onChange={sortPost}
+            defaultValue={'Сортировка'} options={[
+            {value: 'title', name: 'по названию'},
+            {value: 'body', name: 'по описанию'}
+        ]}/>
+            {posts.length
+                ? <PostList removePost={removePost} post={posts} title={'Список постов'}/>
+                : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
+            }
+
+    </div>
+);
 }
 
 export default App;
