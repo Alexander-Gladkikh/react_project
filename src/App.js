3a0c2e7f142eba6b31284ecf2ec9bfd8474ef7e8
Alexander <som17son25@gmail.com>
2023-01-15 12:01:15 +0300

    create component Pagination

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 
 function App() {
@@ -18,20 +20,20 @@ function App() {
 
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [visible, setVisible] = useState(false)
-    const [totalCount, setTotalCount] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
     const [limit, setLimit] = useState(10)
     const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [fetchPost, isPostLoading, postsError] = useFetching(async () => {
+    const [fetchPost, isPostLoading, postsError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page)
         setPosts(response.data)
-        console.log(response.headers['x-total-count'])
-        setTotalCount(response.headers['x-total-count'])
+        const totalCount = (response.headers['x-total-count'])
+        setTotalPages(getPageCount(totalCount, limit))
     })
 
     useEffect(() => {
-        fetchPost()
-    },[])
+        fetchPost(limit, page)
+    }, [])
 
 
     const create = (newPost) => {
@@ -43,12 +45,17 @@ function App() {
         setPosts(posts.filter(el => el.id !== post.id))
     }
 
+    const changePage = (page) => {
+        setPage(page)
+        fetchPost(limit, page)
+    }
+
     return (
         <div className="App">
             <button onClick={fetchPost}>Получить данные</button>
             <MyButton
                 style={{marginTop: '20px'}}
-                 onClick={() => setVisible(true)}
+                onClick={() => setVisible(true)}
             >Добавить пользователя</MyButton>
             <MyModal visible={visible} setVisible={setVisible}>
                 <PostForm create={create}/>
@@ -57,13 +64,15 @@ function App() {
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
             {postsError &&
-            <h1>Произшла ошибка ${postsError}</h1>}
+                <h1>Произшла ошибка ${postsError}</h1>}
             {isPostLoading
-            ? <div style={{display:'flex', marginTop: '20px', justifyContent: 'center'}}><Loader/></div>
-            : <PostList removePost={removePost} post={sortedAndSearchedPosts} title={'Список постов'}/>}
-
-
-
+                ? <div style={{display: 'flex', marginTop: '20px', justifyContent: 'center'}}><Loader/></div>
+                : <PostList removePost={removePost} post={sortedAndSearchedPosts} title={'Список постов'}/>}
+            <Pagination
+                page={page}
+                totalPages={totalPages}
+                changePage={changePage}
+            />
         </div>
     );
 }
